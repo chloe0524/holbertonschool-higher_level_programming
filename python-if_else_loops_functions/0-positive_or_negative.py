da84d4f7d180b8be81bc417169d6bad083d81bb1
chloe0524 <chloco31@gmail.com>
2024-01-22 05:34:46 -0800

    fixed issue with messages

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,8 +4,8 @@ import random
 number = random.randint(-10, 10)
 
 if number > 0:
-    print('is positive')
+    print(f'{number} is positive')
 elif number == 0:
-    print('is zero')
+    print(f'{number} is zero')
 else:
-    print('is negative')
+    print(f'{number} is negative')
