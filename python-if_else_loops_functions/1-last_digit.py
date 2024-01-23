a86b2a221b5eded8caf89ce3604672d56a23687f
chloe0524 <chloco31@gmail.com>
2024-01-23 10:15:28 +0100

    checker issue

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,11 @@
 import random
 
 number = random.randint(-10000, 10000)
-n2 = 'Last digit of'
+string = 'Last digit of'
 
 if (number % 10) > 5:
-    print(f'{n2} {number} is {number % 10} and is greater than 5')
+    print(f'{string} {number} is {number % 10} and is greater than 5')
 elif (number % 10) == 0:
-    print(f'{n2} {number} is {number % 10} and is 0')
-if (number % 10) < 6 and (number % 10) != 0:
-    print(f'{n2} {number} is {number % 10} and is less than 6 and not 0')
+    print(f'{string} {number} is {number % 10} and is 0')
+elif (number % 10) < 6 and (number % 10) != 0:
+    print(f'{string} {number} is {number % 10} and is less than 6 and not 0')
