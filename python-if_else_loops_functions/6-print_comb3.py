44b391ed98ce43345220ecab42c4f6ac1581471a
chloe0524 <chloco31@gmail.com>
2024-01-23 21:06:24 +0100

    try again

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,4 @@ for i in range(8):
     for j in range(i + 1, 10):
         print("{}{}, ".format(i, j), end='')
 
-for i in range(8, 9):
-    for j in range(i, 10):
-        print("{}{}".format(i, j), end=", ")
-print(89)
+print('89')
