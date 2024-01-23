3a6796ea0209a872c46929403da0de3a01566288
chloe0524 <chloco31@gmail.com>
2024-01-23 10:17:10 +0100

    added check if negative valuesor not for number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,14 @@ import random
 number = random.randint(-10000, 10000)
 string = 'Last digit of'
 
-if (number % 10) > 5:
-    print(f'{string} {number} is {number % 10} and is greater than 5')
-elif (number % 10) == 0:
-    print(f'{string} {number} is {number % 10} and is 0')
-elif (number % 10) < 6 and (number % 10) != 0:
-    print(f'{string} {number} is {number % 10} and is less than 6 and not 0')
+if number >= 0:
+    last = number % 10
+else:
+    last = number % -10
+
+if last > 5:
+    print(f'{string} {number} is {last} and is greater than 5')
+elif last == 0:
+    print(f'{string} {number} is {last} and is 0')
+else:
+    print(f'{string} {number} is {last} and is less than 6 and not 0')
