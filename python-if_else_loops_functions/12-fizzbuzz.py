89a011f8ad7ebf646e5014e668f44bbf8fd7c0af
chloe0524 <chloco31@gmail.com>
2024-01-24 10:23:43 +0100

    removed parenthesis

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if (i % 3) == 0:
+        if i % 3 == 0:
             print('Fizz', end=" ")
-        elif (i % 5) == 0:
+        elif i % 5 == 0:
             print('Buzz', end=" ")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        elif i % 3 == 0 and i % 5 == 0:
             print('FizzBuzz', end=" ")
         else:
             print('{}'.format(i), end=" ")
