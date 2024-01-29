b20e71cacc17ccded45a6e47ce1e517c5411cb70
chloe0524 <chloco31@gmail.com>
2024-01-29 23:40:39 +0100

    used if not instead of == 0

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    list = []
+    if my_list is None:
+        return None
+    for i in my_list:
+        if not i % 2:
+            list.append(True)
+        else:
+            list.append(False)
+    return list
