2c2275e1de8be46cb7c56afd0a556850852294d2
chloe0524 <chloco31@gmail.com>
2024-01-30 15:23:48 +0100

    test with changed order

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,27 @@
 
 
 def roman_to_int(roman_string):
-    roman_dico = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000
-      }
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
 
-    num = 0
-    for char in roman_string:
-        num += roman_dico[char]
-    return num
+    result = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        current_value = roman_dict[char]
+
+        if current_value < prev_value:
+            result -= current_value
+        else:
+            result += current_value
+
+        prev_value = current_value
+
+    return result
