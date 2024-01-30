0a643492a8b94093954d73c161c31911af1272dc
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-01-30 17:11:50 +0100

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -14,10 +14,8 @@ def roman_to_int(roman_string):
 
     if not (isinstance(roman_string, str) and roman_string):
         return 0
-
     result = 0
     prev_value = 0
-
     for char in reversed(roman_string):
         current_value = roman_dict[char]
         if current_value < prev_value:
