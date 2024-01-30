078d1f7dfb7fae3ee7e36bd27c697b518e370a4a
chloe0524 <chloco31@gmail.com>
2024-01-30 14:21:42 +0100

    removed copy method to directly work with a_dict

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,5 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    dict_1 = a_dictionary.copy()
-    dict_1[key] = value
-    return dict_1
+    a_dictionary[key] = value
+    return a_dictionary
