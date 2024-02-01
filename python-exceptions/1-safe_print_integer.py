8a4468c44fa215ba2e29bdeab13de7d010a185c7
chloe0524 <chloco31@gmail.com>
2024-02-01 11:49:55 +0100

    forgot to push

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        return (0)  # false
+    return (1)  # true
