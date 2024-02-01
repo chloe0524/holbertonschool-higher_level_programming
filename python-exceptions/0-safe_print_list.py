6cd5abecb091b61d0132679425efb7a7a006f822
chloe0524 <chloco31@gmail.com>
2024-02-01 12:06:12 +0100

    issue val names

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,10 +4,9 @@
 def safe_print_list(my_list=[], x=0):
 
     numerus = 0
-
-    for hi in range(0, x):
+    for i in range(0, x):
         try:
-            print("{}".format(my_list[hi]), end="")
+            print("{}".format(my_list[i]), end="")
         except Exception as e:
             break
         numerus += 1
