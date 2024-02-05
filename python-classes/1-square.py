f02f98e3a56ca8e517e5af74c12d155ede6e586f
chloe0524 <chloco31@gmail.com>
2024-02-05 13:03:20 +0100

    task 1 added comments

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"define empty class square for python."
+
+
+class Square():
+    "def private size attribute"
+    def __init__(self, size):
+        "initalize square using pirvate attribute size as args"
+        self.__size = size
+
+    pass
