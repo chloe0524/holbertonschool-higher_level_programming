37da1e3f4adf83eed96cfe79598075527c1d8ddb
chloe0524 <chloco31@gmail.com>
2024-02-05 18:16:55 +0100

    remove if statement from previous tasks

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,21 +5,16 @@ class Square():
     "define square class for python."
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
-        "getter methods to get size"
+        "getter method to get size"
         return self.__size
 
     @size.setter
     def size(self, value):
-        "setter methods to set size of square"
+        "setter method to set size of square"
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
