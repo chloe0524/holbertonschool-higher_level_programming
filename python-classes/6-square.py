0f61ae5d18d3b397712edb01d313a151388d5610
chloe0524 <chloco31@gmail.com>
2024-02-06 10:25:43 +0100

    sticked the two if conditions together with or and '\'

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -89,8 +89,7 @@ class Square():
         Raises:
             TypeError: If position is not a tuple of 2 non-negative integers
         """
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(i, int) and i >= 0 for i in value):
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not all(isinstance(i, int) and i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
