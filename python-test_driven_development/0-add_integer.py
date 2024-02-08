4a62e662a866748929a7aad2f125f8c6cb523b25
chloe0524 <chloco31@gmail.com>
2024-02-08 12:55:21 +0100

    pushing back previous version

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
     module adding two int together and defining expections
 
@@ -9,8 +8,12 @@
 
 
 def add_integer(a, b=98):
-    if type(a) not in (int, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+
+    a1 = int(a)
+    b1 = int(b)
+
+    return a1 + b1
