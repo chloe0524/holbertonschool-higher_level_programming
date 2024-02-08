7b3d41057985e0b60c4cc22027d55e4150664039
chloe0524 <chloco31@gmail.com>
2024-02-08 12:25:59 +0100

    attempt 1  task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    module adding two int together and defining expections
+
+    Args:
+        a, b = integers
+    """
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a1 = int(a)
+    b1 = int(b)
+
+    return a1 + b1
