efe45551662d9ac52c84161528918e603430471f
chloe0524 <chloco31@gmail.com>
2024-02-09 11:07:46 +0100

    task 2 last change: added doc

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(None))
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
