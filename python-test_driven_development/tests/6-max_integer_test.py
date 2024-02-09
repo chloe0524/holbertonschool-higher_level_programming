144c647d8bcd15a11ceecc0a27d2a95a3f93ea32
chloe0524 <chloco31@gmail.com>
2024-02-09 14:27:40 +0100

    pycode fix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Unittest class for max_integer function"""
     def test_max_integer(self):
@@ -14,7 +15,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([2, 3, -1]), 3)
         self.assertEqual(max_integer([-2, -3, -1]), -1)
         self.assertEqual(max_integer([1]), 1)
-        def test_mixed_positive_and_negative(self):
         self.assertEqual(max_integer([1, 2, 3, -4]), 3)
         self.assertEqual(max_integer([1.5, 2.5, 3.5, 4.5]), 4.5)
         self.assertEqual(max_integer([1, 2.5, 3, 4.5]), 4.5)
