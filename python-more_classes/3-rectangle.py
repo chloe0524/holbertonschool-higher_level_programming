31c95a312fc6623f29b99569b76bcfa13104680c
chloe0524 <chloco31@gmail.com>
2024-02-09 13:19:53 +0100

    fixed indentation issue

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-"""
-module to make a Rectangle based
-class Rectangle that defines a rectangle
-"""
+"""This is a module for a rectangle class."""
 
 
 class Rectangle():
-    """
-    class to draw a rectangle using the Rectangle class
-    """
+  """This is a class for a rectangle object."""
     pass
 
     def __init__(self, width=0, height=0):
-        """
-        initialize rectangle instance
-
-        Args:
-            width(int)
-            height(int)
-        """
+         """This method constructs the object."""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """property to retrieve width of rectangle"""
+        """This property returns the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        get width
+        get width.
 
         Args:
-            value
+            value.
         """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
@@ -43,16 +32,16 @@ class Rectangle():
 
     @property
     def height(self):
-        """property to retrieve height of rectangle"""
+        """property to retrieve height of rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        get height
+        get height.
 
         Args:
-            value
+            value.
         """
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
@@ -61,7 +50,7 @@ class Rectangle():
         self.__height = value
 
     def area(self):
-        """method returns area of rectangle"""
+        """method returns area of rectangle."""
         return self.__height * self.__width
 
     def perimeter(self):
@@ -72,7 +61,7 @@ class Rectangle():
         return ((self.__width + self.__height) * 2)
 
     def __str__(self):
-        """method to print the rectangle with the character #"""
+        """method to print the rectangle with the character #."""
         if self.__width == 0:
             if self.__height == 0:
                 return ('')
