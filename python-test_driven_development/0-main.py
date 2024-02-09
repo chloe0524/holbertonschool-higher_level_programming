e3a4414aa7f9f16de7eddc41955608207df277e2
chloe0524 <chloco31@gmail.com>
2024-02-09 14:32:56 +0100

    changed test to match task

--- python-test_driven_development/0-main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
