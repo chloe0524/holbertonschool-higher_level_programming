4cff200d354b638faed9deb12fee1e1844d81ca3
chloe0524 <chloco31@gmail.com>
2024-02-09 10:24:36 +0100

    pycodestyle fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle():
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
