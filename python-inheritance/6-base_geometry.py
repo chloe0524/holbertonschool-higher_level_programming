c05ccfe0127eead5d637ed4b3f5cce41ab5676c7
chloe0524 <chloco31@gmail.com>
2024-02-12 11:44:55 +0100

    taks 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module used for geometry"""
+
+
+class BaseGeometry():
+    """empty for now"""
+
+    def __init__(self):
+        """initialize shape"""
+
+    def area(self):
+        """method to get area"""
+        raise Exception("area() is not implemented")
