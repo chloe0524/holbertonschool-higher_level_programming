7a3d200205168f607907c0acac776561a0b19ab4
chloe0524 <chloco31@gmail.com>
2024-02-12 11:49:40 +0100

    task without tests

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module used for geometry"""
+
+
+class BaseGeometry():
+    """empty for now"""
+
+    def __init__(self):
+        """initialize shape"""
+
+    def area(self):
+        """method to get area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
