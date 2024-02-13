ea3c01faff6a102a277bfe6c92919a24d6e5319d
chloe0524 <chloco31@gmail.com>
2024-02-13 10:24:51 +0100

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+"""module to check 'list' type"""
+
+
+def is_same_class(obj, a_class):
+
+    """
+    CHECK IF ITS SAME CLASS
+    ARGS:
+        OBJ: ITS AN OBJECT
+        A_CLASSE: ITS A CLASS
+    """
+    return (type(obj) is a_class)
