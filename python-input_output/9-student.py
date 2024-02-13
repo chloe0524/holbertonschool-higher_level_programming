dbda9ef9142b0f0c4555c76b6890bf0f754581a0
chloe0524 <chloco31@gmail.com>
2024-02-13 23:07:00 +0100

    removed import

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,7 +10,14 @@ class Student:
         return self.__dict__
 
     def __init__(self, first_name, last_name, age):
-        """Init a new Student object"""
+        """
+        Init a new Student object
+
+        Args:
+            first_name(str)
+            last_name(str)
+            age(int)
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
