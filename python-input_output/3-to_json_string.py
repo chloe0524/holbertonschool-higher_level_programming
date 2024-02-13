7ad9cbf39f07b6ffe8cfbf279063ccf188a19847
chloe0524 <chloco31@gmail.com>
2024-02-13 17:18:18 +0100

    changed import place

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """module with function to append string to file"""
 
+import json
 
 def to_json_string(my_obj):
-    """Return the JSON string representation of object file"""
+    """Return the JSON string representation of object"""
     return (json.dumps(my_obj))
