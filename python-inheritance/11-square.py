29f7715a650e786a5e82108af1ef69ebdbbfc890
chloe0524 <chloco31@gmail.com>
2024-02-13 13:13:07 +0100

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+This module defines the `BaseGeometry` class.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    BaseGeometry class.
+    """
+
+    def __init__(self, size):
+        """
+        initialize shape
+
+        Ags:
+            width(unsigned int)
+            height(unsigned int)
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """
+        Calculate area of rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """
+        string representation of the square.
+
+        Returns:
+            str: string representation of square
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
