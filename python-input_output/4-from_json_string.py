6a63ef0f018a476737b8399ad3b2305b34a4d40e
chloe0524 <chloco31@gmail.com>
2024-02-13 17:21:32 +0100

    pycode fix

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import json
 """function to return object from json string"""
+
+import json
 
 
 def from_json_string(my_str):
     """Return the JSON string representation of object file"""
-    return json.loads(my_str)
+    return (json.loads(my_str))
