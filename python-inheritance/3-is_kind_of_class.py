105c16990ac0320bb26bad5421c67c2fe2c53295
chloe0524 <chloco31@gmail.com>
2024-02-13 11:00:35 +0100

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+MODULE WITH FUNCTION TO CHECK IF OBJECT IS INSTANCE OF CLASS
+"""
+
+
+def is_same_class(obj, a_class):
+
+    """
+    CHECKS IF OBJECT IS INSTANCE OF SPECIFIED CLASS
+
+    ARGS:
+        OBJ: ITS AN OBJECT
+        A_CLASS: IT HAPPENS TO BE A CLASS
+
+    RETURNS:
+        `TRUE` IF OBJECT IS AN EXACT INSTANCE OF THE SPECIFIED CLASS,
+        `FALSE` OTHERWISE.
+    """
+    return (type(obj) is a_class)
+
+
+def is_kind_of_class(obj, a_class):
+
+    """
+    CHECKS IF OBJECT IS INSTANCE OF SPECIFIED CLASS
+
+    ARGS:
+        OBJ: ITS AN OBJECT
+        A_CLASS: IT HAPPENS TO BE A CLASS
+
+    RETURNS:
+        `TRUE` IF OBJECT IS AN EXACT INSTANCE OF THE SPECIFIED CLASS,
+        `FALSE` OTHERWISE.
+    """
+    return isinstance(obj, a_class)
