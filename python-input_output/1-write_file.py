4c5f38f839dd197a61ab1f7e7f170532c5336ba8
chloe0524 <chloco31@gmail.com>
2024-02-13 16:45:53 +0100

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """method to write file with filename"""
-    with open(filename, "w") as write_file:
-        return write_file.write(text)
+    with open(filename, "w") as write_the_file:
+        return write_the_file.write(text)
