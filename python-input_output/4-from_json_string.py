0ed3a9332270ce8730ba08fa5d3ede806f7351ab
chloe0524 <chloco31@gmail.com>
2024-02-13 17:11:31 +0100

    task 4 load

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""
+module with function to return object
+from json string.
+"""
+
+
+def from_json_string(my_str):
+    """Return the JSON string representation of object file"""
+    return json.loads(my_str)
