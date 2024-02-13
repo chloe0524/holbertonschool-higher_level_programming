e8286e5c1b1f691baca8eb766e49c84e13a33d83
chloe0524 <chloco31@gmail.com>
2024-02-13 22:26:26 +0100

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""returns the dictionary description with simple data structure"""
+
+import json
+
+
+def class_to_json(obj):
+    """returns the dictionary description"""
+    return obj.__dict__
