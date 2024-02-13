45e457701ea763234ed9bab26acd77814eb6c791
chloe0524 <chloco31@gmail.com>
2024-02-13 23:04:29 +0100

    left accidental import

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 """module with function for read+print file contents"""
 
-import json
-
 
 class Student:
     """A class for student infos"""
 
     def __init__(self, first_name, last_name, age):
-        """Init a new Student object"""
+        """
+        Init a new Student object
+
+        Args:
+            first_name(str)
+            last_name(str)
+            age(int)
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns the dictionary description with filters"""
+        """
+        returns the dictionary description with filters
+
+        Args:
+            attrs()
+
+        """
         if attrs is None:
             return self.__dict__
         else:
