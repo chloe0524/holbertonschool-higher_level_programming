1abc5f6c4d8b772a899ecfbce18ea847173cc24e
chloe0524 <chloco31@gmail.com>
2024-02-13 17:06:47 +0100

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""module with function to append string to file"""
+
+
+def to_json_string(my_obj):
+    """Return the JSON string representation of object file"""
+    return json.dumps(my_obj)
