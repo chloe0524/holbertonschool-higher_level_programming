620ce82d6c580f446480342ce00234b3f5bed320
chloe0524 <chloco31@gmail.com>
2024-02-19 13:36:50 +0100

    fixed issue of mixing up two files

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
-"""A class Square"""
+"""class Square"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """A class Square that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialise.
-
-        Args:
-            size (int): the size of square
-            x (int): an integer. Defaults to 0.
-            y (int): an integer. Defaults to 0.
-            id (int): an integer. Defaults to None.
-        """
+        """initialise square"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """getter size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """setter size"""
         self.width = value
         self.height = value
 
     def __str__(self):
-        """Return a string representation of the square."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        """return str representation of square instance"""
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.size))
 
-    def update(self, *args, **kwargs):
-        """That assigns an argument to each attribute"""
-        if args and len(args) > 0:
+        if args:
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
@@ -41,16 +36,23 @@ class Square(Rectangle):
             if len(args) >= 4:
                 self.y = args[3]
         else:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                if key == "size":
-                    self.size = value
-                if key == "x":
-                    self.x = value
-                if key == "y":
-                    self.y = value
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Square"""
-        return {"id": self.id, "x": self.x, "size": self.size, "y": self.y}
+        """
+        Convert the Square instance into a dictionary.
+
+        Return:
+            dict: a dictionary representation of the Square instance.
+        """
+        return {"id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y}
