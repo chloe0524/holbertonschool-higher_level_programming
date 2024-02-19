eb2252c69104a1b7830ef6c5d4e482ca765023c3
chloe0524 <chloco31@gmail.com>
2024-02-19 10:55:11 +0100

    last changes: removed base class to add just the import

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 """class to make """
 
-
-class Base():
-    """class base"""
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from models.base import Base
 
 
 class Rectangle(Base):
