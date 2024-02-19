e53cfc4a99116fbf1cbf2e4b9372069b9f247eb3
chloe0524 <chloco31@gmail.com>
2024-02-19 13:49:13 +0100

    added json string rep

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """class to make """
 
+import json
 
 class Base():
     """class base"""
@@ -12,3 +13,9 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns string rep of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
