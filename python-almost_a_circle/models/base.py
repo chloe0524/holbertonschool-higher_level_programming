6a93d050303b2b531898f409ecfa9cf5ce1d22cb
chloe0524 <chloco31@gmail.com>
2024-02-19 10:29:06 +0100

    issue checker

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class to make """
+
+
+class Base():
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
