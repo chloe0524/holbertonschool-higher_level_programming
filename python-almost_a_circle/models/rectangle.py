bc0f358b6eb565b6094399e09f5899443437611c
chloe0524 <chloco31@gmail.com>
2024-02-19 12:29:33 +0100

    Last changes: requested help wich helped me understand the usage of len in this case

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -110,3 +110,16 @@ class Rectangle(Base):
         """method to print the rectangle with the character #."""
         return ("[Rectangle] ({}) {}/{} - {}/{}"
                 .format(self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args):
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
