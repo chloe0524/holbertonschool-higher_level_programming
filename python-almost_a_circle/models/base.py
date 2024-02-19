1265dffc816be0ffb72c1099a3ba3b4c2961beaa
chloe0524 <chloco31@gmail.com>
2024-02-19 13:50:21 +0100

    added static

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns string rep of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
