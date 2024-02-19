3f2d4d61344bcda14b212452b406d9ee1c319a5b
chloe0524 <chloco31@gmail.com>
2024-02-19 10:57:44 +0100

    Last changes: worked with the wrong error message previously when checking if width and height = 0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -31,7 +31,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -50,7 +50,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
