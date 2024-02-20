b7457795435c270b1cbb568d37262fd99bf16b7a
chloe0524 <chloco31@gmail.com>
2024-02-20 10:13:24 +0100

    issue checker

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -53,3 +53,14 @@ class Base():
             dummy = cls(2)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Function that creates an Object from a JSON file"""
+        filename = cls.__name__ + ".json"
+        if not os.path.exists(filename):
+            return []
+        with open(filename, "r") as f:
+            json_str = f.read()
+            list_dict = cls.from_json_string(json_str)
+            return [cls.create(**d) for d in list_dict]
