01c0a45687c4e459b5221d726dcefbd7b709f8aa
chloe0524 <chloco31@gmail.com>
2024-03-18 14:11:03 +0000

    create server with password and user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create server with user and password:
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
