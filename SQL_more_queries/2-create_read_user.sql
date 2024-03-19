156a0520cea4a311fedb20ca23cf0c115c027d35
chloe0524 <chloco31@gmail.com>
2024-03-19 09:31:00 +0000

    changed syntax

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create server with user and password 2:
-CREATE DATABASE IF NOT EXISTS htbn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY "user_0d_2_pwd";
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
