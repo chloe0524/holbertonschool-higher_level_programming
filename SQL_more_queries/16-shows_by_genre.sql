50f5c7e06a464fbb97978840ee44a23bf43607e9
chloe0524 <chloco31@gmail.com>
2024-03-19 13:25:08 +0000

    task 16 with linked show list

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- lists all shows who are Comedy:
+SELECT tv_shows.title AND tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title AND tv_genres.name;
