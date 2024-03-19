a701db4d724f1ea1abe3e3c3f9b5c8fece2f5a13
chloe0524 <chloco31@gmail.com>
2024-03-19 09:51:18 +0000

    task 6 with states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- database usa + table states:
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+	name VARCHAR(256) NOT NULL
+)
