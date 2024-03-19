1a3fbdc7fd2050b3c2e67c63f6127d1f732a659e
chloe0524 <chloco31@gmail.com>
2024-03-19 12:23:44 +0000

    added null option

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- tvshows listing but task 11:
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
