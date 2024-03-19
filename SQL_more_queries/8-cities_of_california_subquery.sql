a9af925d21ce272f935906835a3a5fe380dad442
chloe0524 <chloco31@gmail.com>
2024-03-19 10:12:07 +0000

    change select to name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- lists all cities of California:
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id;
