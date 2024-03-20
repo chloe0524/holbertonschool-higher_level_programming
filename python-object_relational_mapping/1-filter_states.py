aef6dadf077458808c6034edcaf2276e7a7e53eb
chloe0524 <chloco31@gmail.com>
2024-03-20 11:34:08 +0100

    various pycodesryle fixes

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,10 +10,7 @@ if __name__ == "__main__":
     bdd = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                           passwd=sys.argv[2], db=sys.argv[3])
     cursor = bdd.cursor()
-    cursor.execute(
-        "SELECT DISTINCT id, name FROM states WHERE name LIKE 'n%' "
-        "ORDER BY id ASC"
-    )
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' LIMIT 2")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
