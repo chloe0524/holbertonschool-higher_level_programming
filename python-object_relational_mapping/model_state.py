5c8719daa1681a248aa8c93df3331532da460639
chloe0524 <chloco31@gmail.com>
2024-03-21 10:11:51 +0000

    got mixed up in file names

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """class definition of a state"""
 
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    bdd = sys.argv[3]
-    engine = create_engine(f'mysql+mysqldb://{user}:{password}@localhost/{db}',
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """class state for"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
