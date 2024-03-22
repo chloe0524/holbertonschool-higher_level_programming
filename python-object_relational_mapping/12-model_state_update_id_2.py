a49ca5ca2279b710303133a41bf7203d6ffc7399
chloe0524 <chloco31@gmail.com>
2024-03-22 10:29:10 +0100

    task 12: changes the name of a State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """module changing/updating name of a state from the database """
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, insert
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     bdd_session = Session()
-
-    session.query(State).filter(
-        State.id == 2).update({'name': 'New Mexico'})
-    session.commit()
-    session.close()
+    bdd_session.query(State).filter(
+        State.id == 2).update({"name": "New Mexico"})
+    bdd_session.commit()
+    bdd_session.close()
