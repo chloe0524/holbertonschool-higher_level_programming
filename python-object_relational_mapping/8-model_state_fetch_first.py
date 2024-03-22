61a8f0f478bdc490f74e679323f3e9528671a9b7
chloe0524 <chloco31@gmail.com>
2024-03-22 09:55:03 +0100

    task 8: prints the first State object from the database

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,5 +18,6 @@ if __name__ == "__main__":
 
     if state:
         print("{}: {}".format(state.id, state.name))
-    print('Nothing')
+    else:
+        print('Nothing')
     bdd_session.close()
