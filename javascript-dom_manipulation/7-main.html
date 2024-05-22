1a034e36484c67481410e76bbde6eb90fc858d96
chloe0524 <chloco31@gmail.com>
2024-05-22 17:02:01 +0200

    task 7: Fetch and list Star Wars movie titles using Fetch API

---
+++ javascript-dom_manipulation/7-main.html
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Holberton School</title>
+  </head>
+  <body>
+    <header> 
+      Star Wars movies
+    </header>
+    <br />
+    <ul id="list_movies">
+    </ul>
+    <br />
+    <footer>
+      Holberton School - 2022
+    </footer>
+    <script type="text/javascript" src="7-script.js"></script>
+  </body>
+</html>
---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const data = await (await fetch('https://swapi-api.hbtn.io/api/films/?format=json')).json();
+    data.results.forEach(movie =>
+      document.querySelector('#list_movies').appendChild(Object.assign(document.createElement('li'), { textContent: movie.title }))
+    );
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  }
+});
